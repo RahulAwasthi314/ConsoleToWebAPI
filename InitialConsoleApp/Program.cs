aa6b891e4468ecca0f02828cbd1c5b7f4d501d55
Rahul Awasthi <awasthir314@gmail.com>
2023-12-20 14:48:42 +0530

    Endpoint creation to serve the application on it.

--- InitialConsoleApp/Program.cs
+++ InitialConsoleApp/Program.cs
@@ -1,5 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Reflection.Metadata.Ecma335;
+
+var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/", () => {
+    return "The webApp is running";
+});
 
 app.Run();
